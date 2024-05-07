1d189210c16d420427a2e66a5eded475a83daf94
Your Name <okodee@naver.com>
2024-05-08 01:57:08 +0900

    feat: Add Homepy Repository

---
+++ src/main/java/vom/spring/domain/homepy/HomepyRepository.java
@@ -0,0 +1,20 @@
+package vom.spring.domain.homepy;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class HomepyRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public void save(Homepy homepy) {
+        em.persist(homepy);
+    }
+
+//    public Homepy findByUser(User user) {
+//        return em.find(Homepy.class, user);
+//    }
+}
