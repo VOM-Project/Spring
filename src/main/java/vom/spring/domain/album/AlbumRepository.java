af0a430c7e3df092a8e67cf25a13114f843d730c
Your Name <okodee@naver.com>
2024-05-14 13:54:57 +0900

    feat: Add Album Repository

--- src/main/java/vom/spring/domain/album/AlbumRepository.java
+++ src/main/java/vom/spring/domain/album/AlbumRepository.java
@@ -1,2 +1,19 @@
-package vom.spring.domain.album;public class AlbumRepository {
+package vom.spring.domain.album;
+
+import jakarta.persistence.*;
+import org.springframework.stereotype.Repository;
+import vom.spring.domain.homepy.Homepy;
+
+@Repository
+public class AlbumRepository {
+    @PersistenceContext
+    private EntityManager em;
+
+    public void save(Album album) {
+        em.persist(album);
+    }
+
+//    public Homepy findByHomepy(Integer homepyId) {
+//        return em.find(Homepy.class, homepyId);
+//    }
 }
