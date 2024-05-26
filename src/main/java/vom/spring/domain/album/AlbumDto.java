f452150a750b5b7c092aaa3c15a38fd5b51facde
Your Name <okodee@naver.com>
2024-05-27 00:39:36 +0900

    fix: Modify AlbumDto

--- src/main/java/vom/spring/domain/album/AlbumDto.java
+++ src/main/java/vom/spring/domain/album/AlbumDto.java
@@ -11,12 +11,14 @@ public class AlbumDto {
     @AllArgsConstructor
     @Getter
     static public class Info {
+        Long id;
         String name;
         LocalDateTime created_date;
         String img_url;
 
         public static AlbumDto.Info of(Album album) {
             return Info.builder()
+                    .id(album.getId())
                     .name(album.getName())
                     .created_date(album.getCreatedAt())
                     .img_url(album.getImg_url())
