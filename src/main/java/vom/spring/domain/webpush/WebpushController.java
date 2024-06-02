847d896103e06ab9c007c1735ede10e374bc445b
okod <okodee@naver.com>
2024-06-02 16:53:50 +0900

    feat: Add Webpush Service & Controller

--- src/main/java/vom/spring/domain/webpush/WebpushController.java
+++ src/main/java/vom/spring/domain/webpush/WebpushController.java
@@ -1,16 +1,22 @@
 package vom.spring.domain.webpush;
 
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import vom.spring.domain.touchpoint.TouchpointDto;
-import vom.spring.domain.touchpoint.TouchpointService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
 @Controller
 public class WebpushController {
     private WebpushService webpushService;
@@ -25,5 +31,22 @@ public class WebpushController {
     ) {
         return new ResponseEntity<>(webpushService.getWebpushes(memberId), HttpStatus.OK);
     }
+
+//    @PostMapping("/api/webpush/send")
+//    public String sendNotification(
+//            @RequestBody NotificationRequest request)
+//    {
+//        webpushService.sendNotification(request.getTargetToken(), request.getTitle(), request.getBody());
+//        return "Notification sent successfully!";
+//    }
+//
+//    @Getter
+//    public static class NotificationRequest {
+//        private String targetToken;
+//        private String title;
+//        private String body;
+//
+//        // Getters and Setters
+//    }
 
 }
--- src/main/java/vom/spring/domain/webpush/WebpushService.java
+++ src/main/java/vom/spring/domain/webpush/WebpushService.java
@@ -3,14 +3,21 @@ package vom.spring.domain.webpush;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.HttpClientErrorException;
 import vom.spring.domain.member.domain.Member;
 import vom.spring.domain.member.repository.MemberRepository;
 import vom.spring.domain.touchpoint.Touchpoint;
-import vom.spring.domain.touchpoint.TouchpointDto;
-import vom.spring.domain.touchpoint.TouchpointRepository;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class WebpushService {
@@ -24,23 +31,6 @@ public class WebpushService {
         this.webpushRepository = webpushRepository;
         this.memberRepository = memberRepository;
     }
-    /**
-     * 웹푸쉬 생성
-     */
-    @Transactional
-    public void createWebpush(Long from_member_id, Long to_member_id, Long webcam_id) {
-
-        Member fromMember = memberRepository.findById(from_member_id).get();
-        Member toMember = memberRepository.findById(to_member_id).get();
-
-        webpushRepository.save(
-                Touchpoint.builder()
-                        .createdAt(LocalDateTime.now())
-                        .fromMember(fromMember)
-                        .toMember(toMember)
-                        .build()
-        );
-    }
 
     /**
      * 웹푸쉬 조회
@@ -49,6 +39,40 @@ public class WebpushService {
     public List<WebpushDto> getWebpushes(Long member_id) {
         return webpushRepository.findFromMemberIdsByToMemberId(member_id);
     }
+
+    /**
+     * FCM
+     */
+//    private static final String FCM_API_URL = "https://fcm.googleapis.com/fcm/send";
+//    private static final String SERVER_KEY = ""; // Firebase Console에서 확인한 서버 키
+//
+//    public void sendNotification(String targetToken, String title, String body) {
+//        RestTemplate restTemplate = new RestTemplate();
+//
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.set("Authorization", "key=" + SERVER_KEY);
+//        headers.set("Content-Type", "application/json");
+//
+//        Map<String, Object> notification = new HashMap<>();
+//        notification.put("title", title);
+//        notification.put("body", body);
+//
+//        Map<String, Object> message = new HashMap<>();
+//        message.put("to", targetToken);
+//        message.put("notification", notification);
+//
+//        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(message, headers);
+
+//        ResponseEntity<String> response = restTemplate.exchange(FCM_API_URL, HttpMethod.POST, entity, String.class);
+//        System.out.println("FCM Response: " + response.getBody());
+
+//        try {
+//            ResponseEntity<String> response = restTemplate.postForEntity(FCM_API_URL, entity, String.class);
+//            System.out.println("Response: " + response.getBody());
+//        } catch (HttpClientErrorException e) {
+//            System.err.println("Error: " + e.getStatusCode() + " - " + e.getResponseBodyAsString());
+//        }
+//    }
 
 
 }
