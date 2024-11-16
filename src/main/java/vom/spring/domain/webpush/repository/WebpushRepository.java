ab2788cee0722105c954b43f88d626e9a2064cdc
okod <okodee@naver.com>
2024-11-16 17:24:20 +0900

    feat: createWebpush, deleteWebpushByWebcamId 메서드 추가

--- src/main/java/vom/spring/domain/webpush/repository/WebpushRepository.java
+++ src/main/java/vom/spring/domain/webpush/repository/WebpushRepository.java
@@ -2,7 +2,10 @@ package vom.spring.domain.webpush.repository;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import java.util.Optional;
 import org.springframework.stereotype.Repository;
+import vom.spring.domain.webcam.domain.MemberWebcam;
+import vom.spring.domain.webcam.domain.Webcam;
 import vom.spring.domain.webpush.domain.Webpush;
 import vom.spring.domain.webpush.dto.WebpushDto;
 
@@ -24,5 +27,20 @@ public class WebpushRepository {
                         WebpushDto.class)
                 .setParameter("toMemberId", toMemberId)
                 .getResultList();
+    }
+
+    public Webpush findByWebcamId(Long webcamId) {
+        return em.createQuery("SELECT w FROM Webpush w WHERE w.webcam.id = :webcamId", Webpush.class)
+                .setParameter("webcamId", webcamId)
+                .getSingleResult();
+    }
+
+    public void delete(Webpush webpush) {
+        if (em.contains(webpush)) {
+            em.remove(webpush);
+        } else {
+            Webpush mergedWebpush = em.merge(webpush); // 비영속 상태라면 병합 후 삭제
+            em.remove(mergedWebpush);
+        }
     }
 }
--- src/main/java/vom/spring/domain/webpush/service/WebpushService.java
+++ src/main/java/vom/spring/domain/webpush/service/WebpushService.java
@@ -1,9 +1,14 @@
 package vom.spring.domain.webpush.service;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import vom.spring.domain.member.domain.Member;
 import vom.spring.domain.member.repository.MemberRepository;
+import vom.spring.domain.webcam.domain.Webcam;
+import vom.spring.domain.webpush.domain.Webpush;
 import vom.spring.domain.webpush.dto.WebpushDto;
 import vom.spring.domain.webpush.repository.WebpushRepository;
 
@@ -17,6 +22,9 @@ public class WebpushService {
 //    private final WebcamRepository webcamRepository;
 
     @Autowired
+    private FcmService fcmService;
+
+    @Autowired
     public WebpushService(WebpushRepository webpushRepository, MemberRepository memberRepository) {
         this.webpushRepository = webpushRepository;
         this.memberRepository = memberRepository;
@@ -27,6 +35,30 @@ public class WebpushService {
      */
     public List<WebpushDto> getWebpushes(Long toMemberId) {
         return webpushRepository.findByToMemberId(toMemberId);
+    }
+
+    public void createWebpush(Member fromMember, Member toMember, Webcam webcam) {
+        System.out.println("Enter createWebpush");
+        webpushRepository.save(
+                Webpush.builder()
+                        .createdAt(LocalDateTime.now())
+                        .fromMember(fromMember)
+                        .toMember(toMember)
+                        .webcam(webcam)
+                        .build());
+        try {
+            fcmService.sendMessageTo(toMember.getId());
+        } catch (IOException e) {
+            System.err.println("FCM 메시지 전송 중 오류 발생: " + e.getMessage());
+        }
+        System.out.println("Finish createWebpush");
+    }
+
+    public void deleteWebpushByWebcamId(Long webcamId) {
+        Webpush webpush = webpushRepository.findByWebcamId(webcamId);
+        if (webpush != null) {
+            webpushRepository.delete(webpush);
+        }
     }
 
     /**
